10bc6aab5651cf85f52b6acccdf4b8ef6eddd17e
Elisha Loh <164208133+Elisha-Loh@users.noreply.github.com>
2024-10-24 09:54:17 +0800

    Delete Capt laundry bot directory

--- Capt laundry bot/src/commands/__init__.py
+++
@@ -1,3 +0,0 @@
-from .start import start
-from .select import create_select_menu
-from .status import create_status_command
--- Capt laundry bot/src/commands/start.py
+++
@@ -1,8 +0,0 @@
-import constants
-from telegram import Update
-from telegram.ext import CallbackContext, ConversationHandler
-
-
-async def start(update: Update, context: CallbackContext):
-    await update.effective_message.reply_text(constants.WELCOME_MESSAGE)
-    return ConversationHandler.END
